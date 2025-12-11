50e120d50754a6e7cb41c157ce8cd8c7446b9a25
이태관 <cokid98@naver.com>
2025-12-11 21:57:40 +0900

    Revert "Feat: 전역상태로 관리하는 모달상태를 조건부로 처리"
    This reverts commit 9b4dbe3d3f230868d8623b00003d7dd6094607fe.

--- src/provider/modalProvider.tsx
+++ src/provider/modalProvider.tsx
@@ -1,13 +1,11 @@
 import Modal from "@/components/modal/modal";
-import { useIsModal } from "@/store/modalStore";
 import type { ReactNode } from "react";
 import { createPortal } from "react-dom";
 
 export default function ModalProvider({ children }: { children: ReactNode }) {
-  const isModalOpen = useIsModal();
   return (
     <>
-      {isModalOpen && createPortal(<Modal />, document.body)}
+      {createPortal(<Modal />, document.body)}
       {children}
     </>
   );
