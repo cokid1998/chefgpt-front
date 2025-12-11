9b4dbe3d3f230868d8623b00003d7dd6094607fe
이태관 <cokid98@naver.com>
2025-12-11 17:46:11 +0900

    Feat: 전역상태로 관리하는 모달상태를 조건부로 처리

--- src/provider/modalProvider.tsx
+++ src/provider/modalProvider.tsx
@@ -1,11 +1,13 @@
 import Modal from "@/components/modal/modal";
+import { useIsModal } from "@/store/modalStore";
 import type { ReactNode } from "react";
 import { createPortal } from "react-dom";
 
 export default function ModalProvider({ children }: { children: ReactNode }) {
+  const isModalOpen = useIsModal();
   return (
     <>
-      {createPortal(<Modal />, document.body)}
+      {isModalOpen && createPortal(<Modal />, document.body)}
       {children}
     </>
   );
