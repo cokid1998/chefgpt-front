1451481ece07583fb9e0e49312377e13bf769b56
이태관 <cokid98@naver.com>
2025-12-11 09:24:14 +0900

    Feat: 페이지 전환시 스크롤 초기화 custom hook 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { Toaster } from "@/components/ui/sonner";
 import ModalProvider from "@/provider/modalProvider";
 import RootRouter from "@/route";
+import useScrollToTop from "@/hooks/useScrollToTop";
 
 function App() {
+  useScrollToTop(); // 페이지 전환시 스크롤을 초기화
+
   return (
     <ModalProvider>
       <RootRouter />
