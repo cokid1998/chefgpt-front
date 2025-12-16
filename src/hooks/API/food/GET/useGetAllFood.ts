471620ea40ee90b7e7b972ae83fdc55c9ace2356
이태관 <cokid98@naver.com>
2025-12-16 21:16:28 +0900

    Revert "Refactor: 음식데이터 query-key 정규화 리팩토링"
    This reverts commit 50b66b9f2a280b2864416ab8710ef3d5f0eefcfe.

--- src/hooks/API/food/GET/useGetAllFood.ts
+++ src/hooks/API/food/GET/useGetAllFood.ts
@@ -1,30 +1,15 @@
 import { FOODS_API_URL } from "@/constants/APIUrl";
 import { QUERY_KEYS } from "@/constants/QueryKeys";
 import API from "@/hooks/API/API";
-import {
-  useQuery,
-  useQueryClient,
-  type UseQueryOptions,
-} from "@tanstack/react-query";
+import { useQuery, type UseQueryOptions } from "@tanstack/react-query";
 import { type FoodType } from "@/types/refrigeratorType";
 
-const useGetAllFood = () => {
-  const queryClient = useQueryClient();
-
-  return useQuery({
+const useGetAllFood = () =>
+  useQuery({
     queryKey: QUERY_KEYS.foods,
-    queryFn: async () => {
-      const foods = await API.get<FoodType[]>(FOODS_API_URL);
-      foods.data.forEach((food) => {
-        queryClient.setQueryData<FoodType>(QUERY_KEYS.foodsById(food.id), food);
-      });
-
-      return foods.data.map((food) => food.id);
-    },
-    select: (ids) => {
-      const foods = ids.map((id) =>
-        queryClient.getQueryData<FoodType>(QUERY_KEYS.foodsById(id)),
-      );
+    queryFn: () => API.get<FoodType[]>(FOODS_API_URL),
+    select: (data) => {
+      const foods = data.data;
 
       const countConfig = [
         {
@@ -35,17 +20,17 @@ const useGetAllFood = () => {
         {
           key: "coldCount",
           label: "❄️ 냉장",
-          value: foods.filter((food) => food?.location === "COLD").length,
+          value: foods.filter((food) => food.location === "COLD").length,
         },
         {
           key: "frozenCount",
           label: "🧊 냉동",
-          value: foods.filter((food) => food?.location === "FROZEN").length,
+          value: foods.filter((food) => food.location === "FROZEN").length,
         },
         {
           key: "RoomTempCount",
           label: "🌡️ 실온",
-          value: foods.filter((food) => food?.location === "ROOM_TEMP").length,
+          value: foods.filter((food) => food.location === "ROOM_TEMP").length,
         },
       ];
 
@@ -55,6 +40,5 @@ const useGetAllFood = () => {
       };
     },
   });
-};
 
 export default useGetAllFood;
