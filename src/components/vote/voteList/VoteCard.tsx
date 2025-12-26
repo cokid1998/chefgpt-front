e3a381d323df4c97ae2f54bbca385fc521ea1601
이태관 <cokid98@naver.com>
2025-12-26 19:26:58 +0900

    Feat: 지난 투표 UI 구현

--- src/components/vote/voteList/VoteCard.tsx
+++ src/components/vote/voteList/VoteCard.tsx
@@ -38,32 +38,49 @@ export default function VoteCard({
     submitVote({ voteId, optionName });
   };
 
+  const now = dayjs();
+  const isEndVote = dayjs(endDate).isBefore(now);
+  console.log(optionARatio);
+  console.log(optionBRatio);
+
   return (
-    <div className="rounded-xl border bg-white p-6 shadow transition-all hover:border-green-300 hover:shadow-xl">
+    <div
+      className={`rounded-xl border p-6 shadow transition-all ${
+        isEndVote
+          ? "bg-gray-50"
+          : "bg-white hover:border-green-300 hover:shadow-xl"
+      }`}
+    >
       <div className="flex items-start justify-between">
         <h3 className="mb-2 line-clamp-2 text-lg font-bold text-gray-900">
           {title}
         </h3>
-        <Badge className="rounded-md bg-green-500">{formatDday(endDate)}</Badge>
+        <Badge
+          className={`rounded-md ${isEndVote ? "bg-gray-500" : "bg-green-500"}`}
+        >
+          {formatDday(endDate)}
+        </Badge>
       </div>
       <p className="mb-4 line-clamp-2 text-sm text-gray-500">{description}</p>
 
       <div className="mb-4 space-y-3">
         <button
           onClick={() => handleSubmitVote(id, "A")}
-          className={`relative w-full cursor-pointer overflow-hidden rounded-xl border-2 transition-all hover:border-green-400 hover:bg-green-50 ${
+          className={`relative w-full overflow-hidden rounded-xl border-2 transition-all hover:border-green-400 hover:bg-green-50 ${
             selectedOptions === "A"
               ? "border-green-500 bg-green-50"
               : "border-gray-200 bg-gray-50"
-          }`}
+          } ${isEndVote ? "pointer-events-none border-gray-200! bg-gray-50!" : "cursor-pointer"}`}
         >
           <div
-            className="absolute inset-y-0 left-0 bg-green-200"
+            className={`absolute inset-y-0 left-0 ${
+              isEndVote ? "bg-gray-200" : "bg-green-200"
+            }`}
             style={{ width: `${optionARatio}%` }}
           />
           <div className="relative flex items-center justify-between p-4">
             <div className="flex items-center gap-2">
-              {selectedOptions === "A" && (
+              {selectedOptions === "A" && !isEndVote && (
                 <CheckCircle2
                   className={`size-5 ${selectedOptions === "A" ? "text-green-600" : ""}`}
                 />
@@ -71,7 +88,13 @@ export default function VoteCard({
 
               <span className="font-medium text-gray-900">{optionA}</span>
             </div>
-            <Badge className="rounded-md bg-green-500">{optionARatio}%</Badge>
+            <Badge
+              className={`rounded-md ${
+                isEndVote ? "bg-gray-500" : "bg-green-500"
+              }`}
+            >
+              {optionARatio}%
+            </Badge>
           </div>
         </button>
 
@@ -81,16 +104,18 @@ export default function VoteCard({
             selectedOptions === "B"
               ? "border-green-500 bg-green-50"
               : "border-gray-200 bg-gray-50"
-          }`}
+          } ${isEndVote ? "pointer-events-none border-gray-200! bg-gray-50!" : ""}`}
         >
           <div
-            className="absolute inset-y-0 left-0 bg-green-200"
+            className={`absolute inset-y-0 left-0 ${
+              isEndVote ? "bg-gray-200" : "bg-green-200"
+            }`}
             style={{ width: `${optionBRatio}%` }}
           />
 
           <div className="relative flex items-center justify-between p-4">
             <div className="flex items-center gap-2">
-              {selectedOptions === "B" && (
+              {selectedOptions === "B" && !isEndVote && (
                 <CheckCircle2
                   className={`size-5 ${selectedOptions === "B" ? "text-green-600" : ""}`}
                 />
@@ -98,7 +123,13 @@ export default function VoteCard({
 
               <span className="font-medium text-gray-900">{optionB}</span>
             </div>
-            <Badge className="rounded-md bg-green-500">{optionBRatio}%</Badge>
+            <Badge
+              className={`rounded-md ${
+                isEndVote ? "bg-gray-500" : "bg-green-500"
+              }`}
+            >
+              {optionBRatio}%
+            </Badge>
           </div>
         </button>
       </div>
