060030a7ab45ed32ad0dda3f41c31d64ad29ee49
이태관 <cokid98@naver.com>
2026-01-01 19:41:52 +0900

    Feat: 아티클 작성 페이지 아티클 정보 입력 영역 UI구현

---
+++ src/components/article/ArticleEditorContent.tsx
@@ -0,0 +1,74 @@
+import { BookOpen } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+import useGetArticleCategory from "@/hooks/API/article/GET/useGetArticleCategory";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+export default function ArticleEditorContent() {
+  const { data: categories } = useGetArticleCategory();
+
+  return (
+    <div className="w-1/2 space-y-6">
+      <Accordion type="single" collapsible defaultValue="article-item">
+        <AccordionItem value="article-item" className="rounded-xl shadow-xl">
+          <AccordionTrigger className="cursor-pointer rounded-none bg-linear-to-br from-green-400 to-emerald-500 p-6 text-white">
+            <h1 className="flex items-center gap-2 text-base leading-none font-semibold tracking-tight">
+              <BookOpen className="h-6 w-6" />
+              아티클 정보
+            </h1>
+          </AccordionTrigger>
+
+          <AccordionContent className="space-y-6 p-6">
+            <div>
+              <div className="text-base font-semibold">제목 *</div>
+              <Input
+                placeholder="아티클 제목을 입력하세요"
+                className="mt-2 h-12"
+                required
+              />
+            </div>
+
+            <div>
+              <div className="text-base font-semibold">요약 *</div>
+              <Textarea
+                placeholder="아티클의 간단한 요약을 입력하세요 (2-3문장)"
+                className="mt-2 h-24 resize-none"
+                required
+              />
+            </div>
+
+            <div>
+              <div className="text-base font-semibold">카테고리 *</div>
+              <Select>
+                <SelectTrigger className="mt-2 h-12 w-full">
+                  <SelectValue placeholder="카테고리 선택" />
+                </SelectTrigger>
+                <SelectContent>
+                  {categories?.map((cat) => (
+                    <SelectItem key={cat.id} value={cat.name}>
+                      {cat.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+      <div>asdf</div>
+    </div>
+  );
+}
--- src/page/Article/CreateArticlePage.tsx
+++ src/page/Article/CreateArticlePage.tsx
@@ -1,3 +1,78 @@
+import { Button } from "@/components/ui/button";
+import { ARTICLE } from "@/constants/Url";
+import { ArrowLeft, Sparkles } from "lucide-react";
+import { useNavigate } from "react-router";
+import ArticleEditorContent from "@/components/article/ArticleEditorContent";
+
 export default function CreateArticlePage() {
-  return <div>asdf</div>;
+  const nav = useNavigate();
+
+  return (
+    <div className="min-h-screen bg-linear-to-b from-green-50 to-white">
+      <div className="mx-auto flex-col p-6">
+        <Button
+          variant="ghost"
+          onClick={() => nav(ARTICLE)}
+          className="mb-6 hover:bg-transparent"
+        >
+          <ArrowLeft className="mr-2 h-4 w-4" />
+          목록으로
+        </Button>
+
+        <div className="mb-8">
+          <div className="mb-4 inline-flex items-center gap-2 rounded-full bg-green-200 px-4 py-2">
+            <Sparkles className="h-4 w-4 text-green-600" />
+            <span className="text-sm font-medium text-green-600">
+              요리 정보 작성
+            </span>
+          </div>
+          <h1 className="mb-2 text-4xl font-bold text-gray-900">
+            새 아티클 작성
+          </h1>
+          <p className="text-gray-600">
+            요리와 관련된 유익한 정보를 공유해보세요
+          </p>
+        </div>
+
+        <div className="flex gap-3">
+          <ArticleEditorContent />
+
+          <div className="w-1/2 bg-blue-400">right</div>
+        </div>
+
+        <div className="flex justify-end gap-3 pt-8">
+          <Button
+            type="button"
+            variant="outline"
+            // onClick={() => navigate(createPageUrl("Articles"))}
+            // disabled={createMutation.isPending}
+            className="h-12 px-6"
+          >
+            취소
+          </Button>
+          <Button
+            type="submit"
+            // disabled={createMutation.isPending}
+            className="h-12 bg-linear-to-r from-green-500 to-emerald-500 px-8 hover:from-green-600 hover:to-emerald-600"
+          >
+            {/* {createMutation.isPending ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                저장 중...
+              </>
+            ) : (
+              <>
+                <Sparkles className="mr-2 h-4 w-4" />
+                아티클 작성
+              </>
+            )} */}
+            <>
+              <Sparkles className="mr-2 h-4 w-4" />
+              아티클 작성
+            </>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
