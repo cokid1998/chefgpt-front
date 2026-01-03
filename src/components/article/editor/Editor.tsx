8d75b59556f1dd3607bd2c2a3a097d55e05cf8d3
이태관 <cokid98@naver.com>
2026-01-03 15:27:38 +0900

    Feat: Editor placeholder 구현

--- src/components/article/editor/Editor.tsx
+++ src/components/article/editor/Editor.tsx
@@ -1,6 +1,7 @@
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Toolbar from "@/components/article/editor/Toolbar";
+import { Placeholder } from "@tiptap/extensions";
 
 interface EditorProps {
   handleSetForm: (
@@ -11,7 +12,14 @@ interface EditorProps {
 
 export default function Editor({ handleSetForm }: EditorProps) {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Placeholder.configure({
+        placeholder: "내용을 입력해주세요",
+        emptyEditorClass:
+          "before:content-[attr(data-placeholder)] before:float-left before:text-[#adb5bd] before:h-0 before:pointer-events-none",
+      }),
+    ],
     onUpdate: ({ editor }) => {
       handleSetForm("content", editor.getHTML());
     },
--- src/components/article/editor/EditorViewer.tsx
+++ src/components/article/editor/EditorViewer.tsx
@@ -6,9 +6,19 @@ interface EditorViewerProps {
 
 export default function EditorViewer({ content }: EditorViewerProps) {
   const safeContentHTML = DOMPurify.sanitize(content ?? "");
+
+  const isEmpty =
+    !safeContentHTML ||
+    safeContentHTML === "<p></p>" ||
+    safeContentHTML === "<p><br></p>";
   return (
     <div className="flex-1 border bg-white p-4">
-      <div dangerouslySetInnerHTML={{ __html: safeContentHTML }} />
+      {isEmpty && (
+        <div className="pointer-events-none float-left h-0 text-[#adb5bd]">
+          내용을 입력해주세요
+        </div>
+      )}
+      <div dangerouslySetInnerHTML={{ __html: safeContentHTML }} className="" />
     </div>
   );
 }
