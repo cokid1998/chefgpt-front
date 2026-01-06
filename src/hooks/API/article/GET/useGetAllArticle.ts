052bb526be7cddc445d72616027e20b5a9a0dddf
이태관 <cokid98@naver.com>
2026-01-06 18:54:47 +0900

    comment: 캐시정규화

--- src/hooks/API/article/GET/useGetAllArticle.ts
+++ src/hooks/API/article/GET/useGetAllArticle.ts
@@ -4,6 +4,9 @@ import API from "@/hooks/API/API";
 import { useQuery } from "@tanstack/react-query";
 import type { Article } from "@/types/articleType";
 
+/**
+ * Todo: 캐시정규화
+ */
 const useGetAllArticle = () => {
   return useQuery({
     queryKey: QUERY_KEYS.article.list(),
