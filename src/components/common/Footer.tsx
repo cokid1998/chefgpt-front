7c00454d2a7986722a382becddd34be6200c0421
이태관 <cokid98@naver.com>
2026-01-08 12:39:22 +0900

    Feat: footer영역 깃허브, 블로그아이콘에 tooltip 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,10 @@
 import { Github, PenLine } from "lucide-react";
 import { useState, useEffect } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function Footer() {
   const [isXScroll, setIsXScroll] = useState(false);
@@ -21,14 +26,24 @@ export default function Footer() {
       style={{ paddingBottom: isXScroll ? `8px` : `23px` }}
       className="flex flex-col items-start border-t p-4 text-gray-500"
     >
-      <div className="flex w-full border-b pb-4">
-        <a href="https://github.com/cokid1998" target="_blank">
-          <Github />
-        </a>
+      <div className="flex w-full gap-3 border-b pb-4">
+        <Tooltip>
+          <TooltipTrigger>
+            <a href="https://github.com/cokid1998" target="_blank">
+              <Github />
+            </a>
+          </TooltipTrigger>
+          <TooltipContent>Github</TooltipContent>
+        </Tooltip>
 
-        <a href="https://velog.io/@cokid/posts" target="_blank">
-          <PenLine />
-        </a>
+        <Tooltip>
+          <TooltipTrigger>
+            <a href="https://velog.io/@cokid/posts" target="_blank">
+              <PenLine />
+            </a>
+          </TooltipTrigger>
+          <TooltipContent>블로그</TooltipContent>
+        </Tooltip>
       </div>
 
       <div className="mt-4 flex flex-col">
