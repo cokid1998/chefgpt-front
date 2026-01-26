c6c7903dfb8613f2c1edf98ea3d67ab1051c02c8
이태관 <cokid98@naver.com>
2026-01-26 15:15:37 +0900

    Feat: 레시피 생성 모달 네비게이터 구현

--- src/components/modal/recipe/CreateRecipeModal.tsx
+++ src/components/modal/recipe/CreateRecipeModal.tsx
@@ -2,7 +2,8 @@ import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import RecipeHeader from "@/components/recipe/RecipeIntro/RecipeHeader";
 import RecipeIngredientAccordion from "@/components/recipe/RecipeIntro/RecipeIngredientAccordion";
-import RecipeNavigate from "@/components/recipe/RecipeNavigate";
+import RecipeModalNavigate from "@/components/recipe/RecipeModalNavigate";
+import { useState } from "react";
 
 interface CreateRecipeModalProps {
   recipeScript: string[];
@@ -14,6 +15,7 @@ export default function CreateRecipeModal({
   youtubeUrl,
 }: CreateRecipeModalProps) {
   // Todo: 모달이 꺼질 때 캐시된 script데이터 삭제해야함
+  const [currentStep, setCurrentStep] = useState(0);
 
   return (
     <ScrollArea className="h-200 w-300 overflow-y-auto rounded-2xl bg-white p-4">
@@ -26,7 +28,11 @@ export default function CreateRecipeModal({
 
         <Separator className="my-4 w-full" />
 
-        <RecipeNavigate />
+        <RecipeModalNavigate
+          currentStep={currentStep}
+          setCurrentStep={setCurrentStep}
+          totalLength={recipeScript.length}
+        />
       </div>
     </ScrollArea>
   );
--- src/components/recipe/RecipeModalNavigate.tsx
+++ src/components/recipe/RecipeModalNavigate.tsx
@@ -1,15 +1,39 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import type React from "react";
 
-export default function RecipeNavigate() {
+interface RecipeModalNavigateProps {
+  totalLength: number;
+  currentStep: number;
+  setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function RecipeModalNavigate({
+  totalLength,
+  currentStep,
+  setCurrentStep,
+}: RecipeModalNavigateProps) {
+  console.log(currentStep);
+
+  const nextStep = () => {
+    if (currentStep < totalLength) {
+      setCurrentStep((prev) => prev + 1);
+    }
+  };
+
+  const prevStep = () => {
+    if (currentStep > 0) {
+      setCurrentStep((prev) => prev - 1);
+    }
+  };
+
   return (
     <div className="mt-8 flex items-center justify-between">
       <Button
-        // onClick={prevStep}
-        // disabled={currentStep === 0}
+        onClick={prevStep}
+        disabled={currentStep === 0}
         size="lg"
         className="bg-green-500 text-white hover:bg-green-600 disabled:opacity-30"
-        // disabled
       >
         <ChevronLeft className="mr-2 h-5 w-5" />
         이전
@@ -17,23 +41,14 @@ export default function RecipeNavigate() {
 
       <div className="text-center">
         <div className="mb-1 text-sm opacity-70">
-          {/* {currentStep === 0 ? "시작하기" : `${currentStep} / ${totalSteps}`} */}
-          시작하기
+          {currentStep === 0 ? "시작하기" : `${currentStep} / ${totalLength}`}
         </div>
         <div className="flex gap-1">
-          {/* {[...Array(totalSteps + 1)].map((_, i) => (
-                    <div
-                      key={i}
-                      className={`h-1 rounded-full transition-all ${
-                        i === currentStep ? "w-8 bg-orange-500" : "w-1 bg-white/30"
-                      }`}
-                    />
-                  ))} */}
-          {[...Array(3 + 1)].map((_, i) => (
+          {[...Array(totalLength + 1)].map((_, i) => (
             <div
               key={i}
               className={`h-1 rounded-full transition-all ${
-                i === 1 ? "w-8 bg-green-500" : "w-1 bg-black/30"
+                i === currentStep ? "w-8 bg-green-500" : "w-1 bg-black/30"
               }`}
             />
           ))}
@@ -41,11 +56,10 @@ export default function RecipeNavigate() {
       </div>
 
       <Button
-        // onClick={nextStep}
-        // disabled={currentStep >= totalSteps}
+        onClick={nextStep}
+        disabled={currentStep >= totalLength}
         size="lg"
         className="bg-green-500 text-white hover:bg-green-600 disabled:opacity-30"
-        // disabled
       >
         다음
         <ChevronRight className="ml-2 h-5 w-5" />
