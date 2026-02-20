02ffca134b30392520eff3ba5b3cc815913c3f7c
이태관 <cokid98@naver.com>
2026-02-20 18:16:31 +0900

    Feat: 레시피id에 해당하는 레시피를 가져오는 API 추가

---
+++ src/hooks/API/recipe/GET/useGetOneRecipe.ts
@@ -0,0 +1,17 @@
+import { GET_ONE_RECIPE } from "@/constants/APIUrl";
+import { QUERY_KEYS } from "@/constants/QueryKeys";
+import API from "@/hooks/API/API";
+import { useQuery } from "@tanstack/react-query";
+import type { RecipeType } from "@/types/recipeType";
+
+const useGetOneRecipe = (recipeId: number) => {
+  return useQuery({
+    queryKey: QUERY_KEYS.recipe.byId(recipeId),
+    queryFn: () => API.get<RecipeType>(GET_ONE_RECIPE(recipeId)),
+    select: (data) => data.data || data,
+    staleTime: 1 * 60 * 1000,
+    gcTime: 1 * 60 * 1000,
+  });
+};
+
+export default useGetOneRecipe;
