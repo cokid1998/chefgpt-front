7800831f3e93e6ef05a5a82062ff3ab123ae3aae
이태관 <cokid98@naver.com>
2026-02-20 17:54:06 +0900

    fix: 레시피 리스트 API 전체 카테고리 선택 시 categoryId 쿼리 파라미터 제거

--- src/hooks/API/recipe/GET/useGetRecipe.ts
+++ src/hooks/API/recipe/GET/useGetRecipe.ts
@@ -7,13 +7,16 @@ import type { RecipeType } from "@/types/recipeType";
 const useGetRecipe = (categoryId: number, search: string) => {
   return useQuery({
     queryKey: QUERY_KEYS.recipe.list(categoryId, search),
-    queryFn: () =>
-      API.get<RecipeType[]>(GET_RECIPE, {
+    queryFn: async () => {
+      return API.get<RecipeType[]>(GET_RECIPE, {
         params: {
-          categoryId,
+          // categoryId가 0이면 쿼리스트링에 categoryId가 포함안되게 구현
+          // axios에서 쿼리스트링이 undefined면 url에서 아예 제외시킴
+          categoryId: categoryId === 0 ? undefined : categoryId,
           search,
         },
-      }),
+      });
+    },
     select: (data) => data.data,
   });
 };
