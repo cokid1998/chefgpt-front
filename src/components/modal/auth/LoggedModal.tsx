eb6fe642d7ff4377e7f2bef306cdc43656fd9beb
이태관 <cokid98@naver.com>
2026-03-12 01:19:13 +0900

    Feat: 로그인필요 모달 모바일 반응형 구현

--- src/components/modal/auth/LoggedModal.tsx
+++ src/components/modal/auth/LoggedModal.tsx
@@ -18,18 +18,16 @@ export default function LoggedModal() {
   };
 
   return (
-    <div className="flex w-xl flex-col rounded-sm bg-white p-6">
+    <div className="flex w-full max-w-xl flex-col rounded-sm bg-white p-6 md:mx-0 md:w-xl">
       <h1 className="mb-8 flex justify-between text-2xl font-bold">
         로그인이 필요한 페이지입니다.
         <X onClick={handleCloseModal} className="cursor-pointer" />
       </h1>
-
       <div className="mb-8">
         로그인을 원하지 않는경우
         <br />
         <strong>돌아가기</strong> 버튼을 눌러주세요.
       </div>
-
       <div className="flex justify-end gap-4">
         <Button variant={"outline"} onClick={handleCloseModal}>
           돌아가기
