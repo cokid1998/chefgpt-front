0c3a12d87d0af60bcbe987457c3d62469a442678
이태관 <cokid98@naver.com>
2026-04-01 06:38:59 +0900

    Fix: 글머리기호 높이 삭제

--- src/page/Article/CreateArticle-Page.tsx
+++ src/page/Article/CreateArticle-Page.tsx
@@ -47,12 +47,12 @@ export default function CreateArticlePage() {
       StarterKit.configure({
         bulletList: {
           HTMLAttributes: {
-            class: "pl-4 min-h-96 [&_li_p]:inline list-disc",
+            class: "pl-4 [&_li_p]:inline list-disc",
           },
         },
         orderedList: {
           HTMLAttributes: {
-            class: "pl-4 min-h-96 [&_li_p]:inline list-decimal",
+            class: "pl-4 [&_li_p]:inline list-decimal",
           },
         },
       }),
