bc082b58d8d6df42381088117ec103579fac323d
이태관 <cokid98@naver.com>
2026-04-01 06:56:23 +0900

    Fix: 새로운 아티클이 생성됐을 때 캐시 초기화

--- src/hooks/API/article/POST/usePostArticle.ts
+++ src/hooks/API/article/POST/usePostArticle.ts
@@ -1,10 +1,11 @@
 import { POST_ARTICLE } from "@/constants/APIUrl";
 import { ARTICLE } from "@/constants/Url";
 import API from "@/hooks/API/API";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router";
 import { toast } from "sonner";
 import type { Article } from "@/types/articleType";
+import { QUERY_KEYS } from "@/constants/QueryKeys";
 
 export interface PostArticleReq {
   title: string;
@@ -16,12 +17,16 @@ export interface PostArticleReq {
 }
 
 const usePostArticle = () => {
+  const queryClient = useQueryClient();
+
   const nav = useNavigate();
   return useMutation({
     mutationFn: (payload: PostArticleReq) =>
       API.post<Article>(POST_ARTICLE, payload),
     onSuccess: (data) => {
       const articleId = data.data.id;
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.article.all });
+
       toast.success("글이 추가됐습니다.");
       nav(`${ARTICLE}/${articleId}`);
     },
