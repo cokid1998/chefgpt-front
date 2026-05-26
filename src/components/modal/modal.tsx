ee3fe2d5eb9efc648da4e382663d8db1b23afdb0
이태관 <cokid98@naver.com>
2026-05-26 17:22:42 +0900

    Revert "Refactor: overlay영역을 클릭했을 때 모달이 닫히도록 하는 로직 리팩토링"
    This reverts commit ca89b8f1d292f6bc09ea6526940e45f4a928cff2.

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,6 +1,6 @@
 import { useCloseModal, useIsModal, useModalContent } from "@/store/modalStore";
 import { motion, AnimatePresence, type Variants } from "motion/react";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 const overlayAniVariants: Variants = {
   hidden: { opacity: 0 },
@@ -35,7 +35,6 @@ export default function Modal() {
   const isModalOpen = useIsModal();
   const ModalContent = useModalContent();
   const closeModal = useCloseModal();
-  const modalRef = useRef<HTMLDivElement>(null);
 
   /**
    * 모달이 열렸을 때 세로스크롤 때문에 일어나는 Layout Shift방지
@@ -52,8 +51,8 @@ export default function Modal() {
     };
   }, [isModalOpen]);
 
-  // 모달이 열렸을 때 esc를 들으면 모달이 닫히도록
   useEffect(() => {
+    console.log("asdf");
     const handleEsc = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
         closeModal();
@@ -66,35 +65,11 @@ export default function Modal() {
     };
   }, []);
 
-  // overlay영역을 클릭하면 모달이 닫히도록
-  useEffect(() => {
-    const handleClickOverlay = (e: MouseEvent) => {
-      // Select Portal 영역 클릭 시 무시
-      const target = e.target as Node;
-
-      // data-radix-popper-content-wrapper는 Shadcn Select Portal의 wrapper
-      const isInsidePortal = document
-        .querySelector("[data-radix-popper-content-wrapper]")
-        ?.contains(target);
-
-      if (
-        modalRef.current &&
-        !modalRef.current.contains(target) &&
-        !isInsidePortal
-      ) {
-        closeModal();
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOverlay);
-    return () => document.removeEventListener("mousedown", handleClickOverlay);
-  }, []);
-
   return (
     <AnimatePresence>
       {isModalOpen && (
         <motion.div
-          // onClick={() => closeModal()}
+          onClick={() => closeModal()}
           className="fixed inset-0 z-50 flex h-full w-full items-center justify-center bg-black/80"
           variants={overlayAniVariants}
           initial={"hidden"}
@@ -107,7 +82,6 @@ export default function Modal() {
             animate={"visible"}
             exit={"exit"}
             onClick={(e) => e.stopPropagation()}
-            ref={modalRef}
           >
             {ModalContent}
           </motion.div>
